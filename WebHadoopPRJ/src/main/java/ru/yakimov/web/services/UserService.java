f2357f4857a6e1a45542fd7fa35dfe799c20b925
Vladimir Iakimov <yakimovvn@bk.ru>
2020-04-21 09:06:05 +0300

    AddedSecurity and web configs

---
+++ WebHadoopPRJ/src/main/java/ru/yakimov/web/services/UserService.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ Idea.
+ * User: Якимов В.Н.
+ * E-mail: yakimovvn@bk.ru
+ */
+
+package ru.yakimov.web.services;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+}
